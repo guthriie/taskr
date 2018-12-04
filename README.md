5b3da07c5e8600007935c812b6e4dcf16df6344f
Birge Clark <birgeclark@gmail.com>
2018-12-04 15:26:47 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,44 +1,25 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## set up
 
-## Available Scripts
+`npm install && npm run start`
 
-In the project directory, you can run:
+## goals
 
-### `npm start`
+Fix a few bugs, the app should be able to:
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+- create new task
+- complete/uncomplete task
+- sort based on complete/id/title
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+Refactor:
 
-### `npm test`
+DRY up components
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+split into new components where appropriate
 
-### `npm run build`
+CSS:
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
+throw a little paint on it, nothing fancy
 
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
+## Completion
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+Once completed please create a new github repo and send us a link.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+const restfulAPI = "https://jsonplaceholder.typicode.com/todos";
 
 class App extends Component {
+  constructor(props) {
+    this.state = { filter: "id", tasks: [] };
+  }
+
+  getTasks = () => {
+    fetch(`${restfulAPI}`)
+      .then(response => response.json())
+      .then(tasks => this.setState({ tasks }));
+  };
+
+  uncompleteTask = id => {
+    fetch(`${restfulAPI}/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        completed: false
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        const newTasks = [];
+        for (let i = 0; i < this.tasks.length; i++) {
+          newTasks.push(
+            json.id === this.state.tasks[i].id
+              ? { ...this.state.tasks[i], completed: false }
+              : this.state.tasks[i]
+          );
+        }
+        this.setState({
+          tasks: newTasks
+        });
+      });
+  };
+
+  completeTask = id => {
+    fetch(`${restfulAPI}/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        completed: true
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        const newTasks = [];
+        for (let i = 0; i < this.tasks.length; i++) {
+          newTasks.push(
+            json.id === this.state.tasks[i].id
+              ? { ...this.state.tasks[i], completed: false }
+              : this.state.tasks[i]
+          );
+        }
+        this.setState({
+          tasks: newTasks
+        });
+      });
+  };
+
+  getSortedTasks = () => {
+    return this.state.tasks.sort((a, b) => {
+      if (this.state.filter === "id") {
+        return a.id > b.id ? 1 : -1;
+      }
+
+      if (this.state.filter === "completed") {
+        return a.completed ? -1 : 1;
+      }
+
+      if (this.state.filter === "title") return 1;
+    });
+  };
+
+  setFilterCompleted() {
+    this.setState({ filter: "completed" });
+  }
+
+  setFilterId() {
+    this.setState({ filter: "id" });
+  }
+
+  setFilterTitle() {
+    this.setState({ filter: "title" });
+  }
+
+  addTask = () => {
+    const task = document.getElementById("newTask").value;
+    fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        title: task,
+        userId: 1,
+        completed: false
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(response => response.json())
+      .then(json => this.setState({ tasks: this.state.tasks.concat(json) }));
+  };
+
   render() {
+    this.getTasks();
+    const tasks = this.getSortedTasks();
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main">
+        <div>
+          Add Task: <input id="newTask" />
+          <button onClick={this.addTask}>Add</button>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th onClick={this.setFilterCompleted}>✓</th>
+              <th onClick={this.setFilterId}>ID</th>
+              <th onClick={this.setFilterTitle}>Task</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map(task => (
+              <tr>
+                <td>
+                  {task.completed ? (
+                    <input
+                      onChange={this.uncompleteTask}
+                      checked
+                      type="checkbox"
+                    />
+                  ) : (
+                    <input type="checkbox" onChange={this.completeTask} />
+                  )}
+                </td>
+                <td>{task.id}</td>
+                <td>{task.title}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
